3a67f3b717e9fd1848bedcc48906fe1efffe0f56
Siddhesh Padhye <sidddheshpdy@gmail.com>
2026-03-14 20:37:09 +0530

    fixed add to homescreen issue

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-064dcf68'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2jhptp5869s"
+    "revision": "0.3a4rga16hs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
